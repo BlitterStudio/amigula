ce1ef9fca10e80e68d20e52374a057641537eca2
Dimitris Panokostas <midwan@gmail.com>
2015-05-13 22:48:10 +0200

    initial setup

---
+++ Amigula.Domain.Test/AmigulaTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Amigula.Domain.Test
+{
+    [TestClass]
+    public class AmigulaTest
+    {
+        [TestMethod]
+        public void GetAllGames_ReturnsGamesList()
+        {
+
+        }
+    }
+}

c68d5114146e266d03a5bb0a300037ae17069a90
Dimitris Panokostas <midwan@gmail.com>
2015-06-25 19:21:19 +0200

    Added missing files

---
+++ Amigula.Domain/Services/GamesService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Amigula.Domain.DTO;
+using Amigula.Domain.Interfaces;
+
+namespace Amigula.Domain.Services
+{
+    public class GamesService
+    {
+        private readonly IGamesRepository _gamesRepository;
+
+        public GamesService(IGamesRepository gamesRepository)
+        {
+            _gamesRepository = gamesRepository;
+        }
+
+        public IEnumerable<GamesDto> GetGamesList()
+        {
+            var gamesDto = _gamesRepository.GetGamesList();
+            return gamesDto;
+        }
+    }
+}

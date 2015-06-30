f8e61cac2e2ee47ac6d6bac64afeea8d94204334
Dimitris Panokostas <midwan@gmail.com>
2015-06-30 14:05:51 +0200

    Added a comment to explain why a Unit test might fail (if you don't have Amiga Forever installed).

--- Amigula.AmigaForeverRepository.Test/AmigaForeverTest.cs
+++ Amigula.AmigaForeverRepository.Test/AmigaForeverTest.cs
@@ -17,6 +17,7 @@ namespace Amigula.AmigaForeverRepository.Test
         [TestMethod]
         public void GetEmulatorPaths_ReturnsEmulatorDto()
         {
+            // This test will only pass if you have Amiga Forever installed!
             var result = _amigaForever.GetEmulatorPaths();
 
             Assert.IsNotNull(result);

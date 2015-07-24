f354bebe7bb8f866de551bae0a6c4cfa72333e01
Dimitris Panokostas <midwan@gmail.com>
2015-07-24 14:58:48 +0200

    Added ScreenshotsRepository class

---
+++ Amigula.Domain/Interfaces/IScreenshotsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+
+namespace Amigula.Domain.Interfaces
+{
+    public interface IScreenshotsRepository
+    {
+        BitmapImage LoadImage(string filename);
+    }
+}
---
+++ Amigula.Persistence/ScreenshotsRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media.Imaging;
+using Amigula.Domain.Interfaces;
+
+namespace Amigula.Persistence
+{
+    public class ScreenshotsRepository : IScreenshotsRepository
+    {
+        public BitmapImage LoadImage(string filename)
+        {
+            var gameScreenshot = new BitmapImage();
+            try
+            {
+                gameScreenshot.BeginInit();
+                gameScreenshot.CacheOption = BitmapCacheOption.OnLoad;
+                gameScreenshot.UriSource = new Uri(filename);
+                gameScreenshot.EndInit();
+            }
+            catch (Exception ex)
+            {
+                // something went wrong! Handle accordingly here
+            }
+            return gameScreenshot;
+        }
+    }
+}
